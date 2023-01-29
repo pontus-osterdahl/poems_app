48ddda9b16f851e6b6004d80f165e8a442c7c975
Pontus Österdahl <post1700@student.miun.se>
2023-01-29 22:15:51 +0100

    removed not-functioning securityconfiguration

--- src/main/java/com/example/poems_app/GustavLarssonApplication.java
+++ src/main/java/com/example/poems_app/GustavLarssonApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
-@EnableAutoConfiguration(exclude = {
-        SecurityAutoConfiguration.class
-})
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class GustavLarssonApplication {
 
 	public static void main(String[] args) {
