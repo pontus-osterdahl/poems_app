45f431523743532e84e527e433a325281e3f87e9
Pontus Österdahl <post1700@student.miun.se>
2023-02-08 00:18:22 +0100

    added method to handle saving image to file system

--- src/main/java/com/example/poems_app/ImageService.java
+++ src/main/java/com/example/poems_app/ImageService.java
@@ -1,9 +1,12 @@
 package com.example.poems_app;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class ImageService {
@@ -17,6 +20,17 @@ public class ImageService {
 	
 	public Image saveImage(Image image) {
 		return imgRepository.save(image);
+	}
+	
+	/**
+	 * Method to save an image to a specified location  
+	 * @param image MultipartFile to be saved
+	 * @param path Path to save file to
+	 * @throws IOException
+	 */
+	public void saveImageOnServer(MultipartFile image, String path) throws IOException {
+		File file = new File(path);
+		image.transferTo(file);
 	}
 	
 }
