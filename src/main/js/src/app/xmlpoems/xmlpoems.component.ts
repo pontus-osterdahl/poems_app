0873d74b8f168a9613500afa1c679b48888183d0
Pontus Österdahl <post1700@student.miun.se>
2023-03-30 23:04:58 +0200

    added xmlpoems

---
+++ src/main/js/src/app/xmlpoems/xmlpoems.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-xmlpoems',
+  templateUrl: './xmlpoems.component.html',
+  styleUrls: ['./xmlpoems.component.css']
+})
+export class XmlpoemsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
