8365f7574d16015672630d26c4df37d5d969224d
Pontus Österdahl <post1700@student.miun.se>
2023-04-07 21:20:01 +0200

    added empty test class

--- src/test/java/com/example/poems_app/services/XmlPoemServiceTest.java
+++ src/test/java/com/example/poems_app/services/XmlPoemServiceTest.java
@@ -18,9 +18,6 @@ import com.example.poems_app.xml.XmlPoem;
 
 class XmlPoemServiceTest {
 
-	
-	
-	
 	@Test
 	void shouldParseAndIndexPoem() throws FileNotFoundException, ParserConfigurationException, SAXException, IOException, XPathExpressionException {
 		XmlPoem poem = new XmlPoem();
@@ -28,7 +25,6 @@ class XmlPoemServiceTest {
 		
 		XmlPoemService xmlPoemService = new XmlPoemService();
 		List<ContentItem> contentItems = xmlPoemService.parsePoem(poem);
-		System.out.println(contentItems.get(1).getChoice().getOrig().getText());
 		assertTrue(contentItems.size() == 5);
 		assertTrue(contentItems.get(0).getRelations().get(0).equals("WA_Part01_A01_Antisthenes01_ci1"));
 	}
