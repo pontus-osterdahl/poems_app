f7ae8ec49aac0a0676b04a9644d539fc7aa4061a
Pontus Österdahl <post1700@student.miun.se>
2023-04-24 20:37:39 +0200

    enabled apophthegm creation

--- src/main/java/com/example/poems_app/repositories/RegRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.poems_app.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.poems_app.xml.Reg;
-
-public interface RegRepository extends CrudRepository<Reg, Integer> {
-
-}
